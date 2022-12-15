b749607dc796192c683a54c7d6709a7389156dda
Kalpokas_Simonas <simonas.kalpokas@gmail.com>
2022-12-15 20:54:33 +0200

    Use dfs for day 14

--- solutions/day14.fsx
+++ solutions/day14.fsx
@@ -18,6 +18,24 @@ let grid = [| for _ in 0..max_y -> (Array.zeroCreate (max_x - min_x)) |]
 for t in 0 .. max_x - min_x - 1 do
     grid[max_y][t] <- 1
 
+let rec dfs ((x, y): int * int) (grid: int[][]) : option<int> * int =
+    if grid[y][x] <> 0 then
+        (None, 0)
+    elif y = max_y - 1 then
+        grid[y][x] <- 1
+        (Some(0), 1)
+    else
+        let (down_p1, down_p2) = dfs (x, y + 1) grid
+        let (left_down_p1, left_down_p2) = dfs (x - 1, y + 1) grid
+        let (right_down_p1, right_down_p2) = dfs (x + 1, y + 1) grid
+        let p1s = [ down_p1; left_down_p1; right_down_p1 ]
+        let p2s = [ down_p2; left_down_p2; right_down_p2 ]
+        grid[y][x] <- 1
+        (p1s
+         |> List.tryFindIndex Option.isSome
+         |> Option.map (fun ind -> List.take ind p2s |> List.fold (+) 0 |> (+) <| (Option.get p1s[ind])),
+         (List.reduce (+) p2s) + 1)
+
 for line in lines |> Seq.map (fun coords -> Seq.zip coords (Seq.tail coords)) do
     for (a, b) in line do
         if a[0] <> b[0] then
@@ -29,40 +47,8 @@ for line in lines |> Seq.map (fun coords -> Seq.zip coords (Seq.tail coords)) do
 
 let start_x = 500 - min_x
 let start_y = 0
-let mutable count = 0
-let mutable part_1_is_Done = false
-let mutable bigBreak = false
 
-while not bigBreak do
-    let mutable x = start_x
-    let mutable y = start_y
-    let mutable Break = false
+let part_1, part_2 = dfs (start_x, start_y) grid
 
-    if grid[y][x] <> 0 then
-        bigBreak <- true
-        Break <- true
-
-    while not Break do
-        if grid[y + 1][x] = 0 then
-            y <- y + 1
-        elif grid[y + 1][x - 1] = 0 then
-            y <- y + 1
-            x <- x - 1
-        elif grid[y + 1][x + 1] = 0 then
-            y <- y + 1
-            x <- x + 1
-        else
-            grid[y][x] <- 2
-
-            if not part_1_is_Done && y = max_y - 1 then
-                part_1_is_Done <- true
-                printfn "Part 1: %d" count
-
-            count <- count + 1
-            Break <- true
-
-    if not Break then
-        bigBreak <- true
-
-
-printfn "Part 2: %d" count
+printfn "Part 1: %d" (Option.get part_1)
+printfn "Part 2: %d" part_2
